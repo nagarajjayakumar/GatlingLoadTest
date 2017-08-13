5d727ff357af6743503093a10cf1e2e4aae7e6a4
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2017-08-13 01:31:10 -0400

    GATLLING SPARK LOAD SCALA CODE

---
+++ README.md
@@ -0,0 +1,12 @@
+GATLING SPARK CODE HERE
+=========================
+
+
+To test it out, simply execute the following command:
+
+    $mvn gatling:execute -Dgatling.simulationClass=computerdatabase.BasicSimulation
+
+or simply:
+
+    $mvn gatling:execute
+# GatlingLoadTest
---
+++ src/test/scala/computerdatabase/HttpSimulation1.scala
@@ -0,0 +1,48 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+import scala.concurrent.duration._
+
+class HttpSimulation1 extends Simulation {
+  /* Place for arbitrary Scala code that is to be executed before the simulation begins. */
+  before {
+    println("***** My simulation is about to begin! *****")
+  }
+
+  /* Place for arbitrary Scala code that is to be executed after the simulation has ended. */
+  after {
+    println("***** My simulation has ended! ******")
+  }
+
+  /*
+   * A HTTP protocol builder is used to specify common properties of request(s) to be sent,
+   * for instance the base URL, HTTP headers that are to be enclosed with all requests etc.
+   */
+  val theHttpProtocolBuilder = http
+    .baseURL("http://127.0.0.1:8080/insrun?statement=dataFrame.show(5)")
+
+  /*
+   * A scenario consists of one or more requests. For instance logging into a e-commerce
+   * website, placing an order and then logging out.
+   * One simulation can contain many scenarios.
+   */
+  /* Scenario1 is a name that describes the scenario. */
+  val theScenarioBuilder = scenario("Scenario1")
+    .exec(
+      /* myRequest1 is a name that describes the request. */
+      http("myRequest1")
+        .get("")
+    )
+
+  /*
+   * Define the load simulation.
+   * Here we can specify how many users we want to simulate, if the number of users is to increase
+   * gradually or if all the simulated users are to start sending requests at once etc.
+   * We also specify the HTTP protocol builder to be used by the load simulation.
+   */
+  setUp(
+    theScenarioBuilder.inject(atOnceUsers(10),rampUsers(10) over(5 seconds))
+  ).protocols(theHttpProtocolBuilder)
+}
